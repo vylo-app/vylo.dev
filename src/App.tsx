6e2e3273bf462e8edd8d7b1d27424cc264c92991
kusainovv <kusainovratmir89@gmail.com>
2025-01-02 22:27:15 +0400

    feat ✨: Integrate a Tailwind, integrate a shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,22 @@
-import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((prevCount) => prevCount + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="w-1/2 m-auto">
+      <p className="text-xs text-gray-900 dark:text-white">Vylo</p>
+      <p className="text-sm text-gray-900 dark:text-white">Vylo</p>
+      <p className="text-base text-gray-900 dark:text-white">Vylo</p>
+      <p className="text-lg text-gray-900 dark:text-white">Vylo</p>
+      <p className="text-xl text-gray-900 dark:text-white">Vylo</p>
+      <p className="text-2xl text-gray-900 dark:text-white">Vylo</p>
+      <p className="text-3xl text-gray-900 dark:text-white">Vylo</p>
+      <p className="text-4xl text-gray-900 dark:text-white">Vylo</p>
+      <p className="text-5xl text-gray-900 dark:text-white">Vylo</p>
+      <p className="text-6xl text-gray-900 dark:text-white">Vylo</p>
+      <p className="text-gray-900 text-7xl dark:text-white">Vylo</p>
+      <p className="text-gray-900 text-8xl dark:text-white">Vylo</p>
+      <p className="text-gray-900 text-9xl dark:text-white">Vylo</p>
+    </div>
   );
 }
 
